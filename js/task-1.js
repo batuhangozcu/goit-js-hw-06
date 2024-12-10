93ebb45820862c9da84628731b06d6f5db19ccad
batuhangozcu <batuhangozcu1@gmail.com>
2024-12-10 17:48:38 +0300

    final

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,6 @@ const customer = {
     balance: 24000,
     discount: 0.1,
     orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
     getBalance() {
       return this.balance;
     },
